d618e6a191b4a113a9bf30cb7377c822739f42c4
Muh Isfhani Ghiath <misfahani3@gmail.com>
2021-04-13 14:53:04 +0700

    Initial commit

---
+++ libraries/network/src/main/java/app/isfaaghyth/network/Network.kt
@@ -0,0 +1,37 @@
+package app.isfaaghyth.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object Network {
+
+    private const val TIME_OUT = 30L
+
+    fun builder(url: String = ""): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient())
+            .build()
+    }
+
+    private fun okHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .retryOnConnectionFailure(true)
+            .addInterceptor(createLoggingInterceptor())
+            .pingInterval(TIME_OUT, TimeUnit.SECONDS)
+            .readTimeout(TIME_OUT, TimeUnit.SECONDS)
+            .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
+            .build()
+    }
+
+    private fun createLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+}
