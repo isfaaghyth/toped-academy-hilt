a0287b3c8ad8672068c70e079032216438cb5d4b
isfaaghyth <isfaaghyth@gmail.com>
2021-04-16 09:28:13 +0700

    use singleton instead

--- features/products-dagger/src/main/java/app/topedacademy/productdagger/di/module/ProductDaggerModule.kt
+++ features/products-dagger/src/main/java/app/topedacademy/productdagger/di/module/ProductDaggerModule.kt
@@ -4,20 +4,20 @@ import app.topedacademy.network.Network
 import app.topedacademy.productdagger.data.ProductServices
 import app.topedacademy.productdagger.data.repository.ProductRepository
 import app.topedacademy.productdagger.data.repository.ProductRepositoryImpl
-import app.topedacademy.productdagger.di.ProductDaggerScope
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class ProductDaggerModule {
 
     @Provides
-    @ProductDaggerScope
+    @Singleton
     fun provideProductServices(): ProductServices
             = Network.builder().create(ProductServices::class.java)
 
     @Provides
-    @ProductDaggerScope
+    @Singleton
     fun provideRepository(
         services: ProductServices
     ): ProductRepository = ProductRepositoryImpl(services)
--- features/products-dagger/src/main/java/app/topedacademy/productdagger/di/module/ProductDaggerViewModelModule.kt
+++ features/products-dagger/src/main/java/app/topedacademy/productdagger/di/module/ProductDaggerViewModelModule.kt
@@ -4,17 +4,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import app.topedacademy.abstraction.viewmodel.ViewModelFactory
 import app.topedacademy.abstraction.viewmodel.ViewModelKey
-import app.topedacademy.productdagger.di.ProductDaggerScope
 import app.topedacademy.productdagger.ui.ProductDaggerViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
+import javax.inject.Singleton
 
 @Module
 abstract class ProductDaggerViewModelModule {
 
     @Binds
-    @ProductDaggerScope
+    @Singleton
     abstract fun bindViewModelFactory(
         viewModelFactory: ViewModelFactory
     ): ViewModelProvider.Factory
